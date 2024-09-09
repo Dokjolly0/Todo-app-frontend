25c7964e1ae942ab65389f3ae303c29812aeab7e
Dokjolly0 <alexviolattolibero.it@gmail.com>
2024-09-09 23:00:39 +0200

    Add custom favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
+// Angular imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-
+// Custom components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
@@ -15,7 +16,6 @@ import { AddTodoComponent } from './components/add-todo/add-todo.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { UserAvatarComponent } from './components/user-avatar/user-avatar.component';
-import { provideAnimationsAsync as provideAnimationsAsync_alias } from '@angular/platform-browser/animations/async';
 import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
 
 @NgModule({
@@ -33,20 +33,14 @@ import { EditTodoComponent } from './components/edit-todo/edit-todo.component';
     EditTodoComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
-  // providers: [
-  //   {
-  //     provide: HTTP_INTERCEPTORS,
-  //     useClass: AuthInterceptor,
-  //     multi: true,
-  //   },
-  //   provideAnimationsAsync(),
-  // ],
-  providers: [provideAnimationsAsync_alias()],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
+  ],
+  //providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-function provideAnimationsAsync():
-  | import('@angular/core').Provider
-  | import('@angular/core').EnvironmentProviders {
-  throw new Error('Function not implemented.');
-}
